52d2a9ed3cb0fb6bbe20ddd3c4014df5d7af4252
Ing. Eduardo E. Enríquez R. <rekineke@hotmail.com>
2023-11-03 21:19:51 -0500

    mi primera aplicación ya esta hecha

--- operaconesAritmeticas/js/index.js
+++ operaconesAritmeticas/js/index.js
@@ -1,22 +1,48 @@
-//crear las variables que interactuan con el HTML elementos
-const h1 = document.querySelector("h1");
-const input1 = document.querySelector("#calculo1");
-const input2 = document.querySelector("#calculo2");
-const btn = document.querySelector("#btnCalcular");
-const pResultado = document.querySelector("#resultado");
+//crear variables
+let num1 = document.querySelector("#calculo1");
+let num2 = document.querySelector("#calculo2");
+let total = document.querySelector("#resultado");
+let btnSuma = document.querySelector("#btnSuma");
+let btnResta = document.querySelector("#btnResta");
+let btnMultiplicacion = document.querySelector("#btnMultiplicar");
+let btnDivision = document.querySelector("#btnDividir");
+let btnLimpiar = document.querySelector("#btnClean");
 
-//agregar el evento click al botón
-
-btn.addEventListener("click", calcular);
-
-function calcular() {
-  let num1 = Number(input1.value);
-  let num2 = Number(input2.value);
-
-  if (num1 == "" || num2 == "") {
-    alert("Debes ingresar un número");
-  } else {
-    let resultado = num1 + num2;
-    pResultado.innerHTML = resultado;
-  }
+//funciones
+function limpiar() {
+  num1.value = "";
+  num2.value = "";
+  total.innerText = "";
+  num1.focus();
 }
+
+//sumar
+function sumar() {
+  let suma = Number(num1.value) + Number(num2.value);
+  total.innerText = "La suma es: " + suma;
+}
+
+//restar
+function restar() {
+  let resta = Number(num1.value) - Number(num2.value);
+  total.innerText = "La resta es igual a: " +  resta;
+}
+
+//multiplicar
+function multiplicar() {
+  let multiplicacion = Number(num1.value) * Number(num2.value);
+  total.innerText = "La multiplicación es igual a: " + multiplicacion;
+}
+
+//dividir
+function dividir() {
+  let division = Number(num1.value) / Number(num2.value);
+  total.innerText = "La división es igual a: " + division;
+}
+
+//eventos
+btnSuma.addEventListener("click", sumar);
+btnResta.addEventListener("click", restar);
+btnMultiplicacion.addEventListener("click", multiplicar);
+btnDivision.addEventListener("click", dividir);
+btnLimpiar.addEventListener("click", limpiar);
